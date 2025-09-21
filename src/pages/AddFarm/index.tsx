e4e4492be172c2e14a14eb87d31eb53bf5ae8caa
FehSouza <fernanda.souza142010@gmail.com>
2025-09-20 21:24:35 -0300

    refactor: improve loading fallback in AddFarm component with a custom Fallback component

--- src/pages/AddFarm/index.tsx
+++ src/pages/AddFarm/index.tsx
@@ -5,9 +5,17 @@ const Remote = lazy(
   async () => import('remote/fiap-farm-mfe')
 );
 
+const Fallback = () => {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <span className='text-primary-foreground text-xs'>Loading...</span>
+    </div>
+  );
+};
+
 export const AddFarm = () => {
   return (
-    <Suspense fallback='loading...'>
+    <Suspense fallback={<Fallback />}>
       <Remote />
     </Suspense>
   );
