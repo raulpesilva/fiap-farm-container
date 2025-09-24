c8bd6813e8e3993d70d90887a90b7613bb2fd45b
FehSouza <fernanda.souza142010@gmail.com>
2025-09-24 18:15:32 -0300

    refactor: move useFormSignUp logic into FormSignUp component and remove unused hook

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -2,10 +2,54 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useFormSignUp } from '@/hooks';
 import { cn } from '@/lib/utils';
 import { LoaderCircle } from 'lucide-react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+const useFormSignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleCreateAccount = async () => {
+    if (password !== confirmPassword) return setError('As senhas não coincidem');
+    try {
+      setLoading(true);
+      if (!email || !password || !confirmPassword) return setError('Preencha todos os campos');
+      // const user = await createUserWithEmailAndPassword(auth, email, password);
+      // if (user) dispatchIsAuthenticated(true);
+    } catch (error: any) {
+      console.log('Error creating account:', error);
+      if (error.message.includes('auth/invalid-email')) return setError('E-mail inválido');
+      if (error.message.includes('auth/weak-password')) return setError('A senha deve ter pelo menos 6 caracteres');
+      if (error.message.includes('auth/email-already-in-use')) return setError('E-mail já cadastrado');
+      setError('Sign in failed: ' + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onChange = (setValue: React.Dispatch<React.SetStateAction<string>>, value: string) => {
+    setValue(value);
+    setError('');
+  };
+
+  return {
+    email,
+    password,
+    confirmPassword,
+    error,
+    loading,
+    setEmail,
+    setPassword,
+    setConfirmPassword,
+    handleCreateAccount,
+    onChange,
+  };
+};
 
 export function FormSignUp({ className, ...props }: React.ComponentProps<'div'>) {
   const {
