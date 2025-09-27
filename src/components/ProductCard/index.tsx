6562272f90951fc112955fa2bf7d14023449b984
FehSouza <fernanda.souza142010@gmail.com>
2025-09-27 12:48:55 -0300

    feat: add Icon and ProductCard components for improved product display

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,33 @@
+import type { ProductItem } from '@/@types/product';
+import { useNavigate } from 'react-router-dom';
+import { Icon } from '../Icon';
+import { Button } from '../ui/button';
+import { Card, CardAction, CardContent, CardTitle } from '../ui/card';
+
+type ProductCardProps = Pick<ProductItem, 'id' | 'name' | 'icon' | 'color'>;
+
+export const ProductCard = ({ id, name, icon, color }: ProductCardProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <Card className='w-full h-fit p-0'>
+      <CardAction className='w-full'>
+        <Button
+          variant='ghost'
+          className='w-full h-auto justify-start p-0 rounded-xl text-start cursor-pointer duration-300'
+          onClick={() => navigate(`/produtos/${id}`)}
+        >
+          <CardContent className='w-full flex gap-4 items-center p-6'>
+            <div
+              className='w-8 min-w-8 h-8 flex items-center justify-center rounded-lg bg-background'
+              style={{ color: color }}
+            >
+              <Icon type={icon} />
+            </div>
+            <CardTitle className='text-wrap'>{name}</CardTitle>
+          </CardContent>
+        </Button>
+      </CardAction>
+    </Card>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,8 @@ export * from './EmptyState';
 export * from './Footer';
 export * from './FormSignIn';
 export * from './FormSignUp';
+export * from './Icon';
 export * from './Logo';
 export * from './NavigateMenu';
 export * from './NotificationCard';
+export * from './ProductCard';
