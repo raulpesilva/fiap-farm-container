b4038cc8d34322fc7ff1e174153b7ae165313174
FehSouza <fernanda.souza142010@gmail.com>
2025-09-27 18:19:15 -0300

    feat: add size prop to Icon component for customizable dimensions

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,11 +2,12 @@ import { ICON_MAP } from '@/@types/icon';
 
 interface IconProps extends React.SVGProps<SVGSVGElement> {
   type: keyof typeof ICON_MAP;
+  size?: number;
 }
 
-export const Icon = ({ type, ...props }: IconProps) => {
+export const Icon = ({ type, size, ...props }: IconProps) => {
   const IconComponent = ICON_MAP[type] || ICON_MAP['apple'];
   if (!IconComponent) return null;
 
-  return <IconComponent {...props} />;
+  return <IconComponent {...props} style={{ width: size, height: size }} />;
 };
