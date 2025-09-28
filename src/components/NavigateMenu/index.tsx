0d2ac757b13236f31834b647d58358373f3a001a
raulpesilva <adraulpesilva@gmail.com>
2025-09-28 18:40:52 -0300

    refactor: enhance active link detection in NavigateMenu using matchPath and useMemo

--- src/components/NavigateMenu/index.tsx
+++ src/components/NavigateMenu/index.tsx
@@ -4,16 +4,21 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from '@/components/ui/navigation-menu';
-import { Link, useMatch } from 'react-router-dom';
+import { useMemo } from 'react';
+import { Link, matchPath, useLocation } from 'react-router-dom';
 
 interface MenuItemProps {
   name: string;
   link: string;
-  match?: string;
+  match?: string[];
 }
 
 const MenuItem = ({ name, link, match }: MenuItemProps) => {
-  const isActive = useMatch(match || link);
+  const { pathname } = useLocation();
+
+  const isActive = useMemo(() => {
+    return [...(match || []), link].some((pattern) => matchPath(pattern, pathname));
+  }, [match, pathname]);
 
   return (
     <NavigationMenuItem>
@@ -33,10 +38,10 @@ export const NavigateMenu = () => {
   return (
     <NavigationMenu className='order-4 md:order-1 mx-auto md:mx-0 min-w-full md:min-w-0'>
       <NavigationMenuList>
-        <MenuItem name='Estoques' link='/' />
-        <MenuItem name='Vendas' link='/vendas' match='/vendas*' />
-        <MenuItem name='Metas' link='/metas' match='/metas*' />
-        <MenuItem name='Produtos' link='/produtos' match='/produtos*' />
+        <MenuItem name='Estoques' link='/' match={['/', '/adicionar-estoque']} />
+        <MenuItem name='Vendas' link='/vendas' match={['/vendas*']} />
+        <MenuItem name='Metas' link='/metas' match={['/metas*']} />
+        <MenuItem name='Produtos' link='/produtos' match={['/produtos*']} />
       </NavigationMenuList>
     </NavigationMenu>
   );
