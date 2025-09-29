e982c568634a080817a2f99ccd430336db8f17f5
FehSouza <fernanda.souza142010@gmail.com>
2025-09-29 13:32:03 -0300

    fix: update heading text in AddGoal component for consistency

--- src/pages/AddGoal/index.tsx
+++ src/pages/AddGoal/index.tsx
@@ -3,7 +3,7 @@ import { FormAddGoal } from '@/components';
 export const AddGoal = () => {
   return (
     <section className='w-full max-w-[1232px] h-full flex flex-col items-center gap-8 p-4 mx-auto'>
-      <h1 className='text-xl md:text-2xl font-medium text-primary-foreground text-center'>Cadastrar produto</h1>
+      <h1 className='text-xl md:text-2xl font-medium text-primary-foreground text-center'>Cadastrar meta</h1>
       <FormAddGoal />
     </section>
   );
