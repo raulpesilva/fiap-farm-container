031ac2ee4943c7c3e9752ec35ad14be05376d39d
FehSouza <fernanda.souza142010@gmail.com>
2025-09-29 20:31:04 -0300

    fix: update StocksChart layout for improved responsiveness

--- src/components/StocksChart/index.tsx
+++ src/components/StocksChart/index.tsx
@@ -3,7 +3,7 @@ import { StocksChartPie } from '../StocksChartPie';
 
 export const StocksChart = () => {
   return (
-    <div className='w-full flex flex-col lg:grid lg:grid-cols-2 gap-3'>
+    <div className='w-full flex flex-col lg:flex-row! gap-3'>
       <StocksChartPie />
       <StocksChartArea />
     </div>
